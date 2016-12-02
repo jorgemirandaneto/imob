de1622bdc80668761d5ce894b3900048abf71e15
jhonathas Marreira <jhonathas Marreira>
2016-12-02 18:25:41 -0300

    dialogo de cadastro funcionando gravando no banco

--- src/main/java/br/com/jlib/bean/PrincipalBean.java
+++ src/main/java/br/com/jlib/bean/PrincipalBean.java
@@ -1,6 +1,5 @@
 package br.com.jlib.bean;
 
-
 import java.util.List;
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
@@ -10,13 +9,16 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.event.AjaxBehaviorEvent;
 
 import org.omnifaces.util.Messages;
+import org.primefaces.context.RequestContext;
 
 import br.com.jlib.dao.BairroDAO;
 import br.com.jlib.dao.CidadeDAO;
+import br.com.jlib.dao.EmpresaDAO;
 import br.com.jlib.dao.TipoDAO;
 import br.com.jlib.dao.UsuarioDAO;
 import br.com.jlib.domain.Bairro;
 import br.com.jlib.domain.Cidade;
+import br.com.jlib.domain.Empresa;
 import br.com.jlib.domain.Tipo;
 import br.com.jlib.domain.Usuario;
 
@@ -33,11 +35,19 @@ public class PrincipalBean implements Serializable {
 	private Bairro bairro;
 	private boolean bairroOcultar;
 	private Usuario usuario;
+	private Empresa empresa;
 
-	
-
-	public void novo(){
+	public void novo() {
 		cidade = new Cidade();
+		iniciaUsuario();
+		iniciaEmpresa();
+	}
+
+	public void iniciaEmpresa() {
+		empresa = new Empresa();
+	}
+
+	public void iniciaUsuario() {
 		usuario = new Usuario();
 	}
 
@@ -46,27 +56,26 @@ public class PrincipalBean implements Serializable {
 		try {
 			listarCidades();
 			listarTipo();
+			iniciaUsuario();
+			iniciaEmpresa();
 		} catch (Exception erro) {
 			Messages.addGlobalInfo("Erro ao tentar listar as cidades entre em contado com suporte.");
 			erro.printStackTrace();
 		}
 	}
-	
-	
-	
-	
-	public void listarCidades(){
+
+	public void listarCidades() {
 		try {
 			CidadeDAO cidadeDAO = new CidadeDAO();
 			cidades = cidadeDAO.listar();
-			
+
 		} catch (Exception erro) {
 			Messages.addGlobalInfo("Erro ao tentar listar as cidades entre em contado com suporte.");
 			erro.printStackTrace();
 		}
 	}
-	
-	public void listarTipo(){
+
+	public void listarTipo() {
 		try {
 			TipoDAO tipoDAO = new TipoDAO();
 			tipos = tipoDAO.listar();
@@ -75,8 +84,8 @@ public class PrincipalBean implements Serializable {
 			erro.printStackTrace();
 		}
 	}
-	
-	public void listarBairro(AjaxBehaviorEvent event){
+
+	public void listarBairro(AjaxBehaviorEvent event) {
 		try {
 			setBairroOcultar(false);
 			BairroDAO bairroDAO = new BairroDAO();
@@ -86,22 +95,76 @@ public class PrincipalBean implements Serializable {
 			erro.printStackTrace();
 		}
 	}
-	
-	public void novoUsuario(){
+
+	public void novoUsuario() {
 		try {
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
-			usuarioDAO.salvar(usuario);
-			
-			novo();
-			
-			Messages.addGlobalInfo("Usuário cadastrado com sucesso!");
+			if (usuario.getNome() != null && !usuario.getNome().equals("")) {
+				if (usuario.getEmail() != null && !usuario.getEmail().equals("")) {
+					if (usuario.getUsuario() != null && !usuario.getUsuario().equals("")) {
+						if (usuario.getSenha() != null && !usuario.getSenha().equals("")) {
+							iniciaEmpresa();
+							usuarioDAO.salvar(usuario);
+							novo();
+							Messages.addGlobalInfo("Usuário cadastrado com sucesso!");
+						} else {
+							Messages.addGlobalWarn("Campo senha é obrigatorio!");
+						}
+					} else {
+						Messages.addGlobalWarn("Campo usuario é obrigatorio!");
+					}
+				} else {
+					Messages.addGlobalWarn("Campo email é obrigatorio!");
+				}
+			} else {
+				Messages.addGlobalWarn("Campo nome completo é obrigatorio!");
+			}
+			RequestContext context = RequestContext.getCurrentInstance();
+			context.execute("PF('dilogoCadastrar').show();");
 		} catch (Exception erro) {
 			Messages.addGlobalInfo("Erro ao tentar gravar usuário entre em contado com suporte.");
 			erro.printStackTrace();
 		}
 	}
-	
-// Get e set	
+
+	public void novaEmpresa() {
+		try {
+			EmpresaDAO empresaDAO = new EmpresaDAO();
+			if (empresa.getNome() != null && !empresa.getNome().equals("")) {
+				if (empresa.getCnpj() != null && !empresa.getCnpj().equals("")) {
+					if (empresa.getEndereco() != null && !empresa.getEndereco().equals("")) {
+						if (empresa.getUsuario() != null && !empresa.getUsuario().equals("")) {
+							if (empresa.getSenha() != null && !empresa.getSenha().equals("")) {
+								iniciaUsuario();
+								empresaDAO.salvar(empresa);
+								novo();
+								Messages.addGlobalInfo("Empresa cadastrada com sucesso!");
+							} else {
+								Messages.addGlobalWarn("Campo senha é obrigatorio!");
+							}
+						} else {
+							Messages.addGlobalWarn("Campo usuario é obrigatorio!");
+						}
+					} else {
+						Messages.addGlobalWarn("Campo endereço é obrigatorio!");
+					}
+				} else {
+					Messages.addGlobalWarn("Campo cnpj é obrigatorio!");
+				}
+			} else {
+				Messages.addGlobalWarn("Campo nome é obrigatorio!");
+			}
+			
+			RequestContext context = RequestContext.getCurrentInstance();
+			context.execute("PF('dilogoCadastrar').show();");
+		} catch (Exception erro) {
+			Messages.addGlobalInfo("Erro ao tentar gravar empresa entre em contado com suporte.");
+			erro.printStackTrace();
+		}
+	}
+
+
+	// Get e set
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
@@ -117,7 +180,7 @@ public class PrincipalBean implements Serializable {
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
-	
+
 	public List<Tipo> getTipos() {
 		return tipos;
 	}
@@ -125,7 +188,7 @@ public class PrincipalBean implements Serializable {
 	public void setTipos(List<Tipo> tipos) {
 		this.tipos = tipos;
 	}
-	
+
 	public Tipo getTipo() {
 		return tipo;
 	}
@@ -149,7 +212,7 @@ public class PrincipalBean implements Serializable {
 	public void setBairro(Bairro bairro) {
 		this.bairro = bairro;
 	}
-	
+
 	public boolean isBairroOcultar() {
 		return bairroOcultar;
 	}
@@ -165,7 +228,13 @@ public class PrincipalBean implements Serializable {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
-	
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
 
 }
